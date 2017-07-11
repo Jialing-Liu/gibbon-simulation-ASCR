a015b00bbf49dc8af0b000baacb5c4bd7ec6d24a
Jialing-Liu <liujialing6@outlook.com>
2017-07-11 18:49:37 +0100

    error_density_ascr

---
+++ models/error_density_ascr.R
@@ -0,0 +1,33 @@
+setwd('C:/CR/simulation code/gibbon-simulation-ASCR')
+source('./simulation/ascr_sim.R')
+#specify parameters
+ascr_n_sims<-100
+num_instances <- 50
+sigma<-1000
+spacing<-400
+hardcore<-list(beta=5e-4, hc = 350)
+kappa <- 5
+buffer <- 2000
+true_density <- 0.01
+
+output <- matrix(0,nrow = num_instances, ncol = ascr_n_sims)
+output_error<-output
+for(i in 1:num_instances){
+  for(j in 1:ascr_n_sims){
+  output[i,j] <-ascr_sim(spacing = spacing,
+                       density = true_density,sigma = sigma, hardcore = hardcore,
+                       kappa = kappa, buffer = buffer, out = 'density')
+  j=j+1
+  }
+  output_error[i,]<-output[i,]/true_density
+  true_density <- true_density + 0.01
+  i=i+1
+}
+
+#plot error ratios of density
+plot(y=rowMeans(output_error), x=seq(0.01, 0.5, 0.01), xlab = 'Density/Hectare',
+     ylab = 'Estimate / True Density', xlim = c(0,0.6),ylim=c(0.5,2),
+     main = 'Effect of density on error for ASCR method')
+lm_model<-lm(rowMeans(output_error)~I(seq(0.01,0.5,0.01)^(-2)))
+pred<-predict(lm_model,list(density=seq(0.01,0.5,0.01)))
+lines(seq(0.01,0.5,0.01),pred,lwd=2,col='blue')
