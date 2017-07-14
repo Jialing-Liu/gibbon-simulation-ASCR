9c34a98ff02c72dbe73e02017827bafa6068943a
Jialing-Liu <liujialing6@outlook.com>
2017-07-14 16:07:43 +0100

    plot hardcore process

--- simulation/function of plot territories.R
+++ simulation/function of plot territories.R
@@ -1,11 +1,16 @@
-#plot territories of groups
-spacing=400
-density=0.02
-w = owin(xrange = c(-10*spacing,20*spacing), yrange = c(-10*spacing,20*spacing))
-n <- round(density*(spacing^2)*900/10000)
-# simulate the population with some reasonable guess for density, plot(pop) to verify
-sim_location <- rmh(model= list(cif = 'hardcore', par = list(beta=4e-5,hc=700), w = w), 
-                    start=list(n.start = n),
-                    control=list(nrep=1e8,p=1))
-plot.ppp(sim_location, pch=19, cex=.3, cols = 'red')
-plot.ppp(sim_location%mark%700, markscale = 1, add=T, bg= 'lightblue1',border='black')
+#plot territories of groups(Hardcore process)
+
+plot_hardcore <- function(spacing, density, hardcore = list(beta, hc)){
+  w <- owin(xrange = c(-10*spacing,20*spacing), yrange = c(-10*spacing,20*spacing))
+  n <- round(density*(spacing^2)*900/10000)
+  sim_location <- rmh(model= list(cif = 'hardcore', par = hardcore, w = w), 
+                      start=list(n.start = n),
+                      control=list(nrep=1e7,p=1))
+  
+  plot.ppp(sim_location %mark% hardcore$hc, markscale = 1,
+           bg = rgb(90, 128, 150, 50, maxColorValue=150),
+           show.window = T, show.all = T, legend = F,
+           main = paste('locations of simulation on Hardcore process \n with density=',
+                        density,'(R=', hardcore$hc, ')', sep=''))
+  plot.ppp(sim_location, pch=19, cex=.4, cols = 'red', add=T)
+}
