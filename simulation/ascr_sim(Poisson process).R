57c84450eb46e959d7a8b4a5b1b496af2c31cc32
Jialing-Liu <liujialing6@outlook.com>
2017-07-14 14:27:13 +0100

    1

--- simulation/ascr_sim(Poisson process).R
+++ simulation/ascr_sim(Poisson process).R
@@ -1,4 +1,4 @@
-ascr_sim_poisson <- function(x, spacing, density, sigma, buffer,
+ascr_sim_poisson <- function(spacing, density, sigma, buffer,
                              kappa, out='all') {
   traps <- generate_traps(spacing)
   capture <- capture_history_poisson(traps, density, sigma)
@@ -8,12 +8,12 @@ ascr_sim_poisson <- function(x, spacing, density, sigma, buffer,
   fit_ascr_hn <- fit.ascr(capt=as_capture, traps=traps,
                           fix = list(g0=1),
                           mask = ascr_mask, detfn = 'hn')
-  # if(out=='all'){
-  #   return(as.data.frame(fit_ascr_hn$coefficients))
-  # } else if (out=='density'){
-  #   return(as.data.frame(fit_ascr_hn$coefficients)['D',])
-  # } else if (out == 'sigma'){
-  #   return(as.data.frame(fit_ascr_hn$coefficients)['sigma',])
-  # }
+  if(out=='all'){
+    return(as.data.frame(fit_ascr_hn$coefficients))
+  } else if (out=='density'){
+    return(as.data.frame(fit_ascr_hn$coefficients)['D',])
+  } else if (out == 'sigma'){
+    return(as.data.frame(fit_ascr_hn$coefficients)['sigma',])
+  }
   
 }
