8599c17ea15d96fbfa1ccb719e82d526b74ecef8
Jialing Liu <liujialing6@outlook.com>
2017-07-18 17:07:33 +0100

    the number of generation

--- simulation/generate_data_ascr.R
+++ simulation/generate_data_ascr.R
@@ -9,26 +9,20 @@ capture_history = function(traps, density, sigma,
   ns <- sqrt(nrow(traps)/3)
   xrange = spacing*10
   w = owin(xrange = c(0,10000+distance*(ns-1)), yrange = c(0,10000+distance*(ns-1)))
-  n <- round(density*area(w)/10000)
-  # simulate the population with some reasonable guess for density, plot(pop) to verify
-  if(!is.null(hardcore)){
-    sim_location <- rmh(model= list(cif = 'hardcore', par = hardcore, w = w),
-                        start=list(n.start = n),
-                        control=list(p=1))
-  } else if(!is.null(strausshard)){
-    sim_location <- rmh(model = list(cif = 'straush', par = strausshard, w = w),
-                        start = list(n.start = n),
-                        control = list(p = 1))
-  }
-  m=1
-  while(m != n){
-    pop <- sim.popn(D = density,
+  pop <- sim.popn(D = density,
                     expand.grid(x = c(0,10000+distance*(ns-1)),
                                 y = c(0,10000+distance*(ns-1))),
                     buffer = 0, nsessions = 1)
-    m <- nrow(pop)
+  # simulate the population with some reasonable guess for density, plot(pop) to verify
+  if(!is.null(hardcore)){
+    sim_location <- rmh(model= list(cif = 'hardcore', par = hardcore, w = w),
+                        start=list(n.start = nrow(pop),
+                        control=list(p=1))
+  } else if(!is.null(strausshard)){
+    sim_location <- rmh(model = list(cif = 'straush', par = strausshard, w = w),
+                        start = list(n.start = nrow(pop),
+                        control = list(p = 1))
   }
-
   pop$x <- sim_location$x
   pop$y <- sim_location$y
   out <- sim.capthist(traps=traps, popn = pop, noccasions = 1,
