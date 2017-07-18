239e3f1ffed22d3fe53f675f5ecbfeda4ff34dee
Jialing-Liu <liujialing6@outlook.com>
2017-07-18 10:59:53 +0100

    multiple traps

--- simulation/trap.R
+++ simulation/trap.R
@@ -1,24 +1,17 @@
-#generate traps
-generate_traps<-function(spacing, nx, ny){
-  a<-make.circle(n = 3, spacing = spacing, detector = 'signal', originxy = c(5000,5000))
-  a<-data.frame(A)
-  out <- a
+#generate traps,arranging in nx*ny
+generate_traps<-function(spacing=spacing, nx, ny){
+  out<-make.circle(n = 3, spacing = spacing, detector = 'proximity', originxy = c(5000,5000))
   k=1
   for(i in 1:nx){
     for(j in 1:ny){
-      new <- data.frame(x=1:3,y=1:3)
-      new$x <- a$x + 5000 * (i-1)
-      new$y <- a$y + 5000 * (j-1)
-      assign(LETTERS[k], new)
+      originxy <- c(5000*i,5000*j)
+      new <- make.circle(n = 3, spacing = spacing, detector = 'proximity',
+                         originxy = originxy)
       out <- rbind(out, new)
       k=k+1
-          }
+    }
   }
   out <- out[c(-1,-2,-3),]
   row.names(out)<-1:(nx*ny*3)
-  attr(out,'detector')<-'signal'
-  attr(out,'spacex')<-spacing
-  attr(out,'spacey')<-spacing
-  attr(out,'spacing')<-spacing
   return(out)
 }
