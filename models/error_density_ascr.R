29a46f13ae48689c4901dd054daa40a369da473f
Jialing-Liu <liujialing6@outlook.com>
2017-07-18 10:59:03 +0100

    1

--- models/error_density_ascr.R
+++ models/error_density_ascr.R
@@ -1,20 +1,20 @@
-setwd('C:/CR/simulation code/gibbon-simulation-ASCR')
-source('./simulation/ascr_sim.R')
 #specify parameters
-n_sims<-10
-n_instances <- 100
+n_sims<-5
+n_instances <- 80
+nx<-4
+ny<-4
 sigma<-1000
 spacing<-400
 hardcore<-list(beta=5e-5, hc = 700)
 kappa <- 10
 buffer <- 3000
-true_density <- 0.01
+true_density <- 0.005
 
 density <- matrix(0,nrow = n_instances, ncol = n_sims)
 density_error<-sigma_fit<-kappa_fit<-sigma_error<-kappa_error<-density
 for(i in 1:n_instances){
   for(j in 1:n_sims){
-    output <-ascr_sim(spacing = spacing,density = true_density,sigma = sigma,
+    output <-ascr_sim(spacing = spacing,nx=nx,ny=ny,density = true_density,sigma = sigma,
                       hardcore = hardcore, kappa = kappa, buffer = buffer,
                       out = 'all')
     density[i,j] <- output['D',]
@@ -23,10 +23,11 @@ for(i in 1:n_instances){
     density_error[i,j]<-density[i,j]/true_density
     
   }
-  true_density <- true_density + 0.001
+  true_density <- true_density + 0.0002
 }
-
- #plot error ratios of density
-plot(y=rowMeans(output_error), x=seq(0.01, 0.5, 0.01), xlab = 'Density/Hectare',
-     ylab = 'Estimate / True Density', xlim = c(0,0.6),ylim=c(0.5,2),
-     main = 'Effect of density on error for ASCR method(density=0.04)',pch=16,cex=.8,col='blue')
+sigma_error<-sigma_fit/1000
+ 
+#plot error ratios of density
+plot(y=rowMeans(density_error), x=seq(0.005, 0.0208, 0.0002), xlab = 'Density/Hectare',
+     ylab = 'Estimate / True Density', xlim = c(0.004,0.021),ylim=c(0.5,1.1),
+     main = 'Effect of density on error for ASCR method(sigma=1000)',pch=16,cex=.8,col='blue')
