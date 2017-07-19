1275d5048e08c5261200ce457a3e022e9dce83b3
Jialing-Liu <liujialing6@outlook.com>
2017-07-19 13:08:19 +0100

    simulation of error of density

--- models/error_density_ascr.R
+++ models/error_density_ascr.R
@@ -1,33 +1,38 @@
-#specify parameters
-n_sims<-5
-n_instances <- 80
-nx<-4
-ny<-4
-sigma<-1000
-spacing<-400
-hardcore<-list(beta=5e-5, hc = 700)
-kappa <- 10
-buffer <- 3000
-true_density <- 0.005
-
-density <- matrix(0,nrow = n_instances, ncol = n_sims)
-density_error<-sigma_fit<-kappa_fit<-sigma_error<-kappa_error<-density
-for(i in 1:n_instances){
-  for(j in 1:n_sims){
-    output <-ascr_sim(spacing = spacing,nx=nx,ny=ny,density = true_density,sigma = sigma,
-                      hardcore = hardcore, kappa = kappa, buffer = buffer,
-                      out = 'all')
-    density[i,j] <- output['D',]
-    sigma_fit[i,j] <- output['sigma',]
-    kappa_fit[i,j] <- output['kappa',]
-    density_error[i,j]<-density[i,j]/true_density
-    
-  }
-  true_density <- true_density + 0.0002
-}
-sigma_error<-sigma_fit/1000
- 
+#hardcore, distance=5000, traps in 3*3
+sim1<-sim_density(n_sims=5, n_inst=22, n=3, distance=5000,
+                  sigma=1000,  hardcore=list(beta=5e-5, hc = 700),
+                  kappa=10, buffer=3000, start_d=0.005,by=0.0007)
 #plot error ratios of density
-plot(y=rowMeans(density_error), x=seq(0.005, 0.0208, 0.0002), xlab = 'Density/Hectare',
+plot(y=rowMeans(sim1), x=seq(0.005, 0.0197, 0.0007), xlab = 'Density/Hectare',
+     ylab = 'Estimate / True Density', xlim = c(0.004,0.021),ylim=c(0.5,1.1),
+     main = 'Effect of density on error for ASCR method(sigma=1000)',pch=16,cex=.8,col='blue')
+ 
+
+#hardcore, distance=4500, traps in 3*3
+sim2<-sim_density(n_sims=5, n_inst=22, n=3, distance=4500,
+                  sigma=1000,  hardcore=list(beta=5e-5, hc = 700),
+                  kappa=10, buffer=3000, start_d=0.005,by=0.0007)
+#plot error ratios of density
+plot(y=rowMeans(sim2), x=seq(0.005, 0.0197, 0.0007), xlab = 'Density/Hectare',
+     ylab = 'Estimate / True Density', xlim = c(0.004,0.021),ylim=c(0.5,1.1),
+     main = 'Effect of density on error for ASCR method(sigma=1000)',pch=16,cex=.8,col='blue')
+
+
+#hardcore, distance=4000, traps in 3*3
+sim3<-sim_density(n_sims=5, n_inst=22, n=3, distance=4000,
+                  sigma=1000,  hardcore=list(beta=5e-5, hc = 700),
+                  kappa=10, buffer=3000, start_d=0.005,by=0.0007)
+#plot error ratios of density
+plot(y=rowMeans(sim3), x=seq(0.005, 0.0197, 0.0007), xlab = 'Density/Hectare',
+     ylab = 'Estimate / True Density', xlim = c(0.004,0.021),ylim=c(0.5,1.1),
+     main = 'Effect of density on error for ASCR method(sigma=1000)',pch=16,cex=.8,col='blue')
+
+
+#hardcore, distance=4000, traps in 3*3
+sim4<-sim_density(n_sims=5, n_inst=22, n=3, distance=3500,
+                  sigma=1000,  hardcore=list(beta=5e-5, hc = 700),
+                  kappa=10, buffer=3000, start_d=0.005,by=0.0007)
+#plot error ratios of density
+plot(y=rowMeans(sim4), x=seq(0.005, 0.0197, 0.0007), xlab = 'Density/Hectare',
      ylab = 'Estimate / True Density', xlim = c(0.004,0.021),ylim=c(0.5,1.1),
      main = 'Effect of density on error for ASCR method(sigma=1000)',pch=16,cex=.8,col='blue')
