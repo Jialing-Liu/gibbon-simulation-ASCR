e2276845d10f32c0f66c0a43ae8e4d59957ee9d5
Jialing-Liu <liujialing6@outlook.com>
2017-07-19 13:08:56 +0100

    fix spacing=400

--- simulation/trap.R
+++ simulation/trap.R
@@ -7,14 +7,14 @@ library(circular)
 #generate traps,arranging in n lines and n columns
 #distance is the distance of triangles
 
-generate_traps<-function(spacing=spacing, n, distance){
-  out<-make.circle(n = 3, spacing = spacing, detector = 'proximity',
+generate_traps<-function(n, distance){
+  out<-make.circle(n = 3, spacing = 400, detector = 'proximity',
                    originxy = c(5000,5000))
   k=1
   for(i in 1:n){
     for(j in 1:n){
       originxy <- c(5000+distance*(i-1),5000+distance*(j-1))
-      new <- make.circle(n = 3, spacing = spacing, detector = 'proximity',
+      new <- make.circle(n = 3, spacing = 400, detector = 'proximity',
                          originxy = originxy)
       out <- rbind(out, new)
       k=k+1
