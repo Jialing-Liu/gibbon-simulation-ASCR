dd340935aa97da8b0d0c87553e1ff8220a3ec36a
Jialing-Liu <liujialing6@outlook.com>
2017-07-25 10:09:15 +0100

    some simulations

--- models/error_density_ascr.R
+++ models/error_density_ascr.R
@@ -1,7 +1,7 @@
 #hardcore, distance=5000, traps in 3*3
-sim1<-sim_density(n_sims=5, n_inst=22, n=3, distance=5000,
+sim1<-sim_density(n_sims=10, n_inst=50, n=3, distance=5000,
                   sigma=1000,  hardcore=list(beta=5e-5, hc = 700),
-                  kappa=10, buffer=3000, start_d=0.005,by=0.0007)
+                  kappa=10, buffer=3000, start_d=0.005,by=0.0002)
 #plot error ratios of density
 plot(y=rowMeans(sim1), x=seq(0.005, 0.0197, 0.0007), xlab = 'Density/Hectare',
      ylab = 'Estimate / True Density', xlim = c(0.004,0.021),ylim=c(0.5,1.1),
@@ -9,9 +9,9 @@ plot(y=rowMeans(sim1), x=seq(0.005, 0.0197, 0.0007), xlab = 'Density/Hectare',
  
 
 #hardcore, distance=4500, traps in 3*3
-sim2<-sim_density(n_sims=5, n_inst=22, n=3, distance=4500,
+sim2<-sim_density(n_sims=10, n_inst=50, n=3, distance=4500,
                   sigma=1000,  hardcore=list(beta=5e-5, hc = 700),
-                  kappa=10, buffer=3000, start_d=0.005,by=0.0007)
+                  kappa=10, buffer=3000, start_d=0.005,by=0.0002)
 #plot error ratios of density
 plot(y=rowMeans(sim2), x=seq(0.005, 0.0197, 0.0007), xlab = 'Density/Hectare',
      ylab = 'Estimate / True Density', xlim = c(0.004,0.021),ylim=c(0.5,1.1),
@@ -19,9 +19,9 @@ plot(y=rowMeans(sim2), x=seq(0.005, 0.0197, 0.0007), xlab = 'Density/Hectare',
 
 
 #hardcore, distance=4000, traps in 3*3
-sim3<-sim_density(n_sims=5, n_inst=22, n=3, distance=4000,
+sim3<-sim_density(n_sims=10, n_inst=50, n=3, distance=4000,
                   sigma=1000,  hardcore=list(beta=5e-5, hc = 700),
-                  kappa=10, buffer=3000, start_d=0.005,by=0.0007)
+                  kappa=10, buffer=3000, start_d=0.005,by=0.0002)
 #plot error ratios of density
 plot(y=rowMeans(sim3), x=seq(0.005, 0.0197, 0.0007), xlab = 'Density/Hectare',
      ylab = 'Estimate / True Density', xlim = c(0.004,0.021),ylim=c(0.5,1.1),
@@ -29,9 +29,9 @@ plot(y=rowMeans(sim3), x=seq(0.005, 0.0197, 0.0007), xlab = 'Density/Hectare',
 
 
 #hardcore, distance=4000, traps in 3*3
-sim4<-sim_density(n_sims=5, n_inst=22, n=3, distance=3500,
+sim4<-sim_density(n_sims=10, n_inst=50, n=3, distance=3500,
                   sigma=1000,  hardcore=list(beta=5e-5, hc = 700),
-                  kappa=10, buffer=3000, start_d=0.005,by=0.0007)
+                  kappa=10, buffer=3000, start_d=0.005,by=0.0002)
 #plot error ratios of density
 plot(y=rowMeans(sim4), x=seq(0.005, 0.0197, 0.0007), xlab = 'Density/Hectare',
      ylab = 'Estimate / True Density', xlim = c(0.004,0.021),ylim=c(0.5,1.1),
