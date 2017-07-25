48b4ec5887997de54723021a87a7abe84891950f
Jialing-Liu <liujialing6@outlook.com>
2017-07-25 10:09:34 +0100

    1

--- simulation/generate_data_ascr.R
+++ simulation/generate_data_ascr.R
@@ -9,9 +9,9 @@ capture_history = function(traps, density, sigma,
   ns <- sqrt(nrow(traps)/3)
   w = owin(xrange = c(0,10000+distance*(ns-1)), yrange = c(0,10000+distance*(ns-1)))
   pop <- sim.popn(D = density,
-                    expand.grid(x = c(0,10000+distance*(ns-1)),
+                  expand.grid(x = c(0,10000+distance*(ns-1)),
                                 y = c(0,10000+distance*(ns-1))),
-                    buffer = 0, nsessions = 1)
+                  buffer = 0, nsessions = 1)
   # simulate the population with some reasonable guess for density, plot(pop) to verify
   if(!is.null(hardcore)){
     sim_location <- rmh(model= list(cif = 'hardcore', par = hardcore, w = w),
