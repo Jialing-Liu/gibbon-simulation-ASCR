ec570e9fa00a8195769e7ca8e7078a07f16d2586
Jialing-Liu <liujialing6@outlook.com>
2017-07-28 16:05:43 +0100

    modify with bearing simulation

--- simulation/ascr_sim.R
+++ simulation/ascr_sim.R
@@ -1,21 +1,23 @@
+#combine functions to simulation
+#it returns estimated parameters of ascr and secr(attribution of it)
+#'buffer' is the buffer of capture area when fit models
 ascr_sim <- function(n, distance, density, sigma, hardcore=NULL,
-                     strausshard = NULL, kappa, buffer, out='all') {
+                     strausshard = NULL,simulate,kappa=NULL,buffer) {
   traps <- generate_traps(n,distance)
   capture <- capture_history(traps, density, sigma, hardcore, strausshard)
-  as_capture <- convert_to_ascr(capture, kappa)
+  as_capture <- convert_to_ascr(capture,simulate,kappa)
   ascr_mask <- make.mask(traps = traps, buffer,
-                         type="trapbuffer",nx=64) 
+                         type="trapbuffer",nx=64)
+  fit_secr_hn <- secr.fit(capture, buffer=buffer,detectfn = 'HN',
+                          fix=list(g0=1),trace=F,verify = T,
+                          binomN = 1)
   fit_ascr_hn <- fit.ascr(capt=as_capture, traps=traps,
                           fix = list(g0=1),
                           mask = ascr_mask, detfn = 'hn')
-  if(out=='all'){
-    return(as.data.frame(fit_ascr_hn$coefficients))
-  } else if (out=='density'){
-    return(as.data.frame(fit_ascr_hn$coefficients)['D',])
-  } else if (out == 'sigma'){
-    return(as.data.frame(fit_ascr_hn$coefficients)['sigma',])
-  }
   
+  out<-as.data.frame(fit_ascr_hn$coefficients)
+  attr(out,'secr')<-fit_secr_hn
+  return(out)
 }
 
 
